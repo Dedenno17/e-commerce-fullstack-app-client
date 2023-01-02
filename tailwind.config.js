8c960029d5f9bd20b29cf5cfd818e9ec9e0b9a54
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-01-02 14:00:31 +0700

    mengconfigurasi color & font

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, './src/**/*.{js,ts,jsx,tsx}'),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryPink: '#FB2E86',
+        primaryRed: '#F92545',
+        primaryBlue: '#2E19C4',
+        primaryNavyBlue: '#151875',
+        primaryPurple: '#7E33E0',
+        primarySkyBlue: '#F2F5FF',
+        primaryPantonePurple: '#C7BCD9',
+        secondaryNavyBlue: '#2A3BA8',
+        secondaryBlue: '#151875',
+        secondaryPurple: '#9F64B4',
+      },
+      fontFamily: {
+        josefin: ['Josefin Sans'],
+        lato: ['Lato'],
+      },
+    },
+  },
+  plugins: [],
+};
