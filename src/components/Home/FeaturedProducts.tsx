2f18abd5ae1452802814bb2288d8c4b13269ee2e
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2023-01-07 16:31:04 +0700

    set animasi hover untuk featured products

--- src/components/Home/FeaturedProducts.tsx
+++ src/components/Home/FeaturedProducts.tsx
@@ -7,6 +7,9 @@ import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/a11y';
 import Image from 'next/image';
+import { BsCart } from 'react-icons/bs';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { BiSearchAlt } from 'react-icons/bi';
 
 const FeaturedProducts: React.FC = () => {
   const [pageActive, setPageActive] = useState<number>(0);
@@ -41,8 +44,9 @@ const FeaturedProducts: React.FC = () => {
           .slice(0, 6)
           .map((item: Product) => (
             <SwiperSlide key={item.id} className="flex">
-              <div className="h-[95%] w-[95%] rounded-lg shadow-simetri m-auto w-full">
-                <div className="w-full h-[65%] bg-primarySkyBlue flex p-4">
+              <div className="h-[95%] w-[95%] rounded-lg shadow-simetri m-auto w-full group">
+                {/* IMAGE */}
+                <div className="w-full h-[65%] bg-primarySkyBlue flex p-4 relative overflow-hidden">
                   <div className="relative m-auto w-28 h-32">
                     <Image
                       src={item.img}
@@ -53,9 +57,22 @@ const FeaturedProducts: React.FC = () => {
                       className="w-[60%]"
                     />
                   </div>
+                  <div className="w-[60%] absolute -top-0 left-0 flex p-1 justify-around items-center">
+                    <span className="inline-block w-6 h-6 flex justify-center items-center rounded-full hover:bg-primarySkyBlue hover:brightness-95 text-xs text-secondaryBlue hover:text-primaryNavyBlue scale-0 opacity-0 transition duration-700 group-hover:scale-100 group-hover:opacity-100 ">
+                      <BsCart />
+                    </span>
+                    <span className="inline-block w-6 h-6 flex justify-center items-center rounded-full hover:bg-primarySkyBlue hover:brightness-95 text-xs text-secondaryBlue hover:text-primaryNavyBlue scale-0 opacity-0 transition duration-700 group-hover:scale-100 group-hover:opacity-100 ">
+                      <MdFavoriteBorder />
+                    </span>
+                    <span className="inline-block w-6 h-6 flex justify-center items-center rounded-full hover:bg-primarySkyBlue hover:brightness-95 text-xs text-secondaryBlue hover:text-primaryNavyBlue scale-0 opacity-0 transition duration-700 group-hover:scale-100 group-hover:opacity-100 ">
+                      <BiSearchAlt />
+                    </span>
+                  </div>
                 </div>
-                <div className="w-full h-[35%] p-2 flex flex-col items-center justify-between">
-                  <h2 className="text-sm text-primaryPink font-bold font-lato">
+
+                {/* DESCRIPTION */}
+                <div className="w-full h-[35%] p-2 flex flex-col items-center justify-between transition duration-700 group-hover:bg-primaryPurple">
+                  <h2 className="text-sm text-primaryPink font-bold font-lato transition duration-700 group-hover:text-white">
                     {item.title}
                   </h2>
                   <div className="w-full flex justify-center items-center p-1 gap-1 ">
@@ -72,10 +89,10 @@ const FeaturedProducts: React.FC = () => {
                       />
                     ))}
                   </div>
-                  <span className="text-xs text-primaryBlue font-josefin font-bold">
+                  <span className="text-xs text-primaryBlue font-josefin font-bold transition duration-700 group-hover:text-white">
                     code: {item.id}
                   </span>
-                  <span className="text-xs text-primaryBlue font-josefin font-bold">
+                  <span className="text-xs text-primaryBlue font-josefin font-bold transition duration-700 group-hover:text-white">
                     ${item.price}
                   </span>
                 </div>
@@ -89,8 +106,8 @@ const FeaturedProducts: React.FC = () => {
         {[1, 2, 3, 4, 5, 6].map((item: number) => (
           <span
             key={Math.random() + item + ''}
-            className={`inline-block rounded-full w-[10%] py-[1px] border-[1px] border-primaryPink ${
-              pageActive === item ? 'bg-primaryPink' : 'bg-transparent'
+            className={`inline-block rounded-full w-[10%] py-[1.5px] bg-primaryPink ${
+              pageActive === item ? 'opacity-100' : 'opacity-[0.3]'
             }`}
           />
         ))}
